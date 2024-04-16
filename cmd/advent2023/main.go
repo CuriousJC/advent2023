c16f013389be4e187c82c963e8e038737693d573
Justin Crosby <justin.crosby@gmail.com>
2024-04-16 18:36:30 -0400

    discovery: github must serve out source behind a CDN with some caching, give external references som time to pick up updates

--- cmd/advent2023/main.go
+++ cmd/advent2023/main.go
@@ -1,10 +1,10 @@
 package main
 
-//import 	"github.com/curiousjc/golang-learning/pkg/conversation"
-
 import (
 	"fmt"
 
+	"github.com/curiousjc/golang-learning/pkg/chorus"
+	"github.com/curiousjc/golang-learning/pkg/conversation"
 	"github.com/curiousjc/golang-learning/pkg/goodbye"
 	"github.com/curiousjc/golang-learning/pkg/random"
 )
@@ -15,6 +15,7 @@ func main() {
 	fmt.Println("Here comes a random number from our external package...")
 	fmt.Println(random.Get())
 	fmt.Println(goodbye.Goodbye())
-	//fmt.Println(conversation.Get())
+	fmt.Println(conversation.Get())
+	fmt.Println(chorus.Get())
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/curiousjc/advent2023
 
 go 1.21.5
 
-require github.com/curiousjc/golang-learning v0.0.0-20240416201435-554678e6b16d
+require github.com/curiousjc/golang-learning v0.0.0-20240416205054-83bf26bc4a3b
