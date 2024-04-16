6801257a1cab0a2089d3a9b4f4b304489020b3bb
Justin Crosby <justin.crosby@gmail.com>
2024-04-16 16:35:55 -0400

    capturing working goodbye reference

--- cmd/advent2023/main.go
+++ cmd/advent2023/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/curiousjc/golang-learning/pkg/goodbye"
 	"github.com/curiousjc/golang-learning/pkg/random"
 )
 
@@ -11,5 +12,6 @@ func main() {
 
 	fmt.Println("Here comes a random number from our external package...")
 	fmt.Println(random.Get())
-	//fmt.Println(goodbye.Goodbye())
+	fmt.Println(goodbye.Goodbye())
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/curiousjc/advent2023
 
 go 1.21.5
 
-require github.com/curiousjc/golang-learning v0.0.0-20240416194957-2bcb9b493fb6
+require github.com/curiousjc/golang-learning v0.0.0-20240416201435-554678e6b16d
